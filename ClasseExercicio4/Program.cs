266d4ba5c892041dfa7020f4ec23089c25a55e62
Vitor Paiola <vitinho.souza1234@gmail.com>
2023-10-23 13:55:06 -0300

    Adicionar arquivos de projeto.

---
+++ ClasseExercicio4/Program.cs
@@ -0,0 +1,12 @@
+﻿using ClasseExercicio4;
+using System.Globalization;
+
+Retangulo ret = new Retangulo();
+
+Console.WriteLine("Entre com a largura e altura do retângulo:");
+ret.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+ret.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.WriteLine("AREA = " + ret.Area().ToString("F2", CultureInfo.InvariantCulture));
+Console.WriteLine("PERÍMETRO = " + ret.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+Console.WriteLine("DIAGONAL = " + ret.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
